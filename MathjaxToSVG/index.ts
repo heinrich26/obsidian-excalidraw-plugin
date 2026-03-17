f899219cb2ca75f86e90df450952350dce36a4b6
henrich26 <hendrik-horstmann@o2mail.de>
2026-03-17 17:50:18 +0100

    Use MathJax from Obsidian for TeX parsing
    This will use the MathJax instance used by Obsidian to convert TeX to MathML. Excalidraw now only converts from MathML to SVG. This way, we can reuse the global preamble and respect any configuration changes to Obsidian's MathJax config (loading of additional packages etc.). For demonstration/testing, you may use my own plugin: [obsidian-mathhax](https://github.com/heinrich26/obsidian-mathhax), which injects the `siunitx` package.
    The preamble setting is now quite redundant, yet I've left it there to allow loading an additional one.

--- MathjaxToSVG/index.ts
+++ MathjaxToSVG/index.ts
@@ -1,17 +1,30 @@
-import {mathjax} from "mathjax-full/js/mathjax";
-import {TeX} from 'mathjax-full/js/input/tex.js';
-import {SVG} from 'mathjax-full/js/output/svg.js';
-import {LiteAdaptor, liteAdaptor} from 'mathjax-full/js/adaptors/liteAdaptor.js';
-import {RegisterHTMLHandler} from 'mathjax-full/js/handlers/html.js';
-import {AllPackages} from 'mathjax-full/js/input/tex/AllPackages.js';
+import { mathjax } from "mathjax-full/js/mathjax";
+import { MathML } from 'mathjax-full/js/input/mathml.js';
+import { SVG } from 'mathjax-full/js/output/svg.js';
+import { type LiteAdaptor, liteAdaptor } from 'mathjax-full/js/adaptors/liteAdaptor.js';
+import { RegisterHTMLHandler } from 'mathjax-full/js/handlers/html.js';
+import { MathDocument } from "mathjax-full/js/core/MathDocument";
+import { OptionList } from "mathjax-full/js/util/Options";
+import { MathJaxObject } from "mathjax-full/js/components/startup";
+import type { ParserConfiguration } from "mathjax-full/js/input/tex/Configuration";
+import type { CHTML } from "mathjax-full/js/output/chtml";
+import { HandlerType } from "mathjax-full/js/input/tex/MapHandler";
 import { customAlphabet } from "nanoid";
+
+declare global {
+  interface Window {
+    MathJax: {
+      tex2mml: (tex: string, options: OptionList) => string;
+    } & MathJaxObject
+  }
+}
 
 type DataURL = string & { _brand: "DataURL" };
 type FileId = string & { _brand: "FileId" };
 const fileid = customAlphabet("1234567890abcdef", 40);
 
 let adaptor: LiteAdaptor;
-let html: any;
+let html: MathDocument<any, unknown, unknown>;
 let preamble: string;
 
 function svgToBase64(svg: string): string {
@@ -20,10 +33,40 @@ function svgToBase64(svg: string): string {
   // Convert the string to UTF-8 and handle non-Latin1 characters
   const encodedData = encodeURIComponent(cleanSvg)
     .replace(/%([0-9A-F]{2})/g,
-      (match, p1) => String.fromCharCode(parseInt(p1, 16))
+      (_, p1) => String.fromCharCode(parseInt(p1, 16))
     );
     
   return `data:image/svg+xml;base64,${btoa(encodedData)}`;
+}
+
+function getMJXSubHandlers() {
+  return (((window.MathJax.startup.output as unknown as CHTML<any, any, any>)
+    .math.inputJax as any)
+    .configuration as ParserConfiguration)
+    .handlers;
+}
+
+function getCommandMaps(): [HandlerType, string, Map<string, any>][] {
+  const keys: [HandlerType, string][] = [["macro", "new-Command"], ["environment", "new-Environment"]];
+  const handlers = getMJXSubHandlers();
+
+  return keys.map(([key, handler]) => [key, handler, new Map((
+    (handlers
+      .get(key)
+      .retrieve(handler) as any).map as Map<string, any>)
+  )]);
+}
+
+function restoreCommandMaps(maps: [HandlerType, string, Map<string, any>][]) {
+  const handlers = getMJXSubHandlers();
+
+  maps.forEach(([key, handler, map]) => {
+    const targetMap = (handlers.get(key).retrieve(handler) as any).map as Map<string, any>;
+    targetMap.clear();
+    for (let [k, v] of map.entries()) {
+      targetMap.set(k, v);
+    }
+  });
 }
 
 async function getImageSize(src: string): Promise<{ height: number; width: number }> {
@@ -46,34 +89,35 @@ export async function tex2dataURL(
   created: number;
   size: { height: number; width: number };
 }> {
-  let input: TeX<unknown, unknown, unknown>;
+  let inputMl: MathML<unknown, unknown, unknown>;
   let output: SVG<unknown, unknown, unknown>;
 
   if(!adaptor) {
     if (plugin) {
-      const file = plugin.app.vault.getAbstractFileByPath(plugin.settings.latexPreambleLocation || "preamble.sty");
+      // don't load a fallback preamble, because we're using the global latex preamble.
+      const file = plugin.app.vault.getAbstractFileByPath(plugin.settings.latexPreambleLocation);
       preamble = file ? await plugin.app.vault.read(file) : null;
     }
     adaptor = liteAdaptor();
     RegisterHTMLHandler(adaptor);
-    input = new TeX({
-      packages: AllPackages,
-      ...(preamble ? {
-        inlineMath: [['$', '$']],
-        displayMath: [['$$', '$$']]
-      } : {}),
-    });
+
+    inputMl = new MathML();
     output = new SVG({ fontCache: "local" });
-    html = mathjax.document("", { InputJax: input, OutputJax: output });
+    html = mathjax.document("", { InputJax: inputMl, OutputJax: output });
   }
 
   try {
-    const node = html.convert(
+    /* reuse the MathJax from obsidian to make use of it's the configuration
+       (inherits the preamble and any other loaded modules).
+       We save the previous macros to avoid leaking the preamble into the global scope. */
+    const macros = getCommandMaps();
+    const node = html.convert(window.MathJax.tex2mml(
       preamble ? `${preamble}\n${tex}` : tex,
       { display: true, scale }
-    );
+    ));
+    restoreCommandMaps(macros);
     const svg = new DOMParser().parseFromString(adaptor.innerHTML(node), "image/svg+xml").firstChild as SVGSVGElement;
-    
+
 	//https://github.com/zsviczian/obsidian-excalidraw-plugin/issues/2195
 	//https://stackoverflow.com/a/77181931
 	let styleNode = document.createElement('style');
@@ -88,11 +132,10 @@ export async function tex2dataURL(
       const img = svgToBase64(svg.outerHTML);
       svg.width.baseVal.valueAsString = (svg.width.baseVal.valueInSpecifiedUnits * 10).toFixed(3);
       svg.height.baseVal.valueAsString = (svg.height.baseVal.valueInSpecifiedUnits * 10).toFixed(3);
-      const dataURL = svgToBase64(svg.outerHTML);
       return {
         mimeType: "image/svg+xml",
         fileId: fileid() as FileId,
-        dataURL: dataURL as DataURL,
+        dataURL: img as DataURL,
         created: Date.now(),
         size: await getImageSize(img),
       };
